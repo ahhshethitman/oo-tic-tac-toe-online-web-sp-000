bfa9e6759d531f0279eae9392d4d8eee3a1a578e
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-27 19:23:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,8 +25,8 @@ class TicTacToe
           puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
       end
       
-      def input_to_index 
-        
+      def input_to_index (user_input)
+          user_input.to_i - 1
       end 
       
       def move 
